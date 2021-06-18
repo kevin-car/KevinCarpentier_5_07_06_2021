413deaf4a3329825893c7e40131f2619b4ebbd62
Kevin Carpentier <kevin.carpentier.dev@gmail.com>
2021-06-18 12:29:13 +0200

    Page produit avec menu déroulant

--- relier.js
+++ relier.js
@@ -1,26 +1,3 @@
-// Fonction Principale 
-Main()
-async function Main(){
-    const articles = await getArticle()
-    const i =  recupI(articles)
-    displayElements(i, articles)
-    GetArticleforBasket(articles)
-}
-
-// Ma fetch principale, recupération brut des Articles de l'API 
-function getArticle(){
-    return fetch("http://localhost:3000/api/teddies/")
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (articles) {
-        // return article
-            return(articles)
-    })
-    .catch(function (error) {
-        console.log("erreur du get article")
-    })
-}
 
 // fonction permettant d'obtenir un paramètre url
 function $_GET(param) {
@@ -28,95 +5,110 @@ function $_GET(param) {
 	window.location.href.replace( location.hash, '' ).replace( 
 		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
 		function( m, key, value ) { // callback
-			vars[key] = value !== undefined ? value : ''
+			vars[key] = value !== undefined ? value : '';
 		}
-	)
+	);
 
 	if ( param ) {
-		return vars[param] ? vars[param] : null	
+		return vars[param] ? vars[param] : null;	
 	}
 	return vars;
 }
+
 // Obtenir le Data-Id de l'URL
-var data = $_GET('data-id')
+const dataID = $_GET('data-id')
 
-
-// fonction qui recupère l'index dans l'API
-function recupI (articles){
-    for(i=0 ; i<articles.length; i++){
-        // console.log(articles[i]._id)
-        if(articles[i]._id == data){
-            return(i)
-        }
-    }
+let article = {
+    exemple: "exemple ok",
 }
 
+// Fonction Principale 
+Main()
+    async function Main(){
+        const myarticles = await getArticle()
+        displayElements(myarticles)
+    }
+
+// Ma fetch principale, recupération brut des Articles de l'API 
+function getArticle(){
+    return fetch("http://localhost:3000/api/teddies/" + dataID)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (myarticles) {
+        return (myarticles)
+    })
+    .catch(function (error) {
+        console.log("erreur du get article")
+    })
+}
 
 // Modifier les Elements 
-function displayElements(i, articles){
+function displayElements(myarticles){
     // Image
     let imageAModifier = document.querySelector(".photoArticle img")
-        imageAModifier.setAttribute("src", articles[i].imageUrl)
-        imageAModifier.setAttribute("alt", articles[i].description)
+        imageAModifier.setAttribute("src", myarticles.imageUrl)
+        imageAModifier.setAttribute("alt", "00")
     // Texte 
     let nomAModifier = document.querySelector(".blocDescription--nomProduit")
-        nomAModifier.innerHTML = articles[i].name
+        nomAModifier.innerHTML = myarticles.name
     // description
     let description = document.querySelector(".blocDescription--description")
-        description.innerHTML = articles[i].description    
+        description.innerHTML = myarticles.description    
     // Prix
     let prixAModifier = document.querySelector(".blocQuantite__prix")
-        prixAModifier.innerHTML = "Prix unitaire : <strong>" + articles[i].price/100 + "<strong> €"
+        prixAModifier.innerHTML = "Prix unitaire : <strong>" + myarticles.price/100 + "<strong> €"
     // Titre 
     let titreAModifier = document.querySelector("title")
-        titreAModifier.innerHTML = "Orinoco : " +  articles[i].name 
+        titreAModifier.innerHTML = "Orinoco : " +  myarticles.name 
 
-    let couleursAAjouter = document.querySelector(".blocDescription--couleurs")
+    let couleursAAjouter = document.querySelector(".blocSelecteur__selectionCouleur")
+    console.log(couleursAAjouter)
 
-        for(j=0; j<articles[i].colors.length ; j++){
-
-            let colorName = articles[i].colors[j]
-
+        for(j=0; j<myarticles.colors.length ; j++){
+            let colorName = myarticles.colors[j]
             couleursAAjouter.innerHTML += 
             ` 
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="${colorName}" value="option2">
-                <label class="form-check-label" for="inlineRadio2"> ${colorName}  </label>
-            </div>
+            <option value=${colorName} style="text-align: center">${colorName}</option>
             `
+            document.querySelector(".blocSelecteur__exempleCouleur").style.backgroundColor= colorName;
+
+
+
         }
 }
 
+// changement de la couleur de l'exmple 
+    
+// // creation de la classe Article (pour ajout au panier)
+// class ArticlePAnier{
+//     constructor(i, couleur, quantite){
+//         this.i = i
+//         this.couleur = couleur 
+//         this.quantite = quantite
+//     }
+// }
 
-// creation de la classe Article (pour ajout au panier)
-class ArticlePAnier{
-    constructor(i, couleur, quantite){
-        this.i = i
-        this.couleur = couleur 
-        this.quantite = quantite
-    }
-}
+// function GetArticleforBasket(articles){
 
-function GetArticleforBasket(articles){
-
-const monBouton = document.querySelector("button")
+// const monBouton = document.querySelector("button")
 
 
 
 
-// recupérer les Quantités, couleur et Article en question
-let TheArticle = articles[i].name
-console.log(TheArticle)
-// recupération de la couleur 
-let couleurChoisie = document.getElementsByClassName("form-check-input")
-const couleur = function(){
-    for(p=0 ; p < couleurChoisie.length ; p++)
-    if(couleurChoisie[p].checked == false){
-        return couleurChoisie[p].id
-    }
-}
+// // recupérer les Quantités, couleur et Article en question
+// let TheArticle = articles[i].name
+// console.log(TheArticle)
+// // recupération de la couleur 
+// let couleurChoisie = document.getElementsByClassName("form-check-input")
+// const couleur = function(){
+//     for(p=0 ; p < couleurChoisie.length ; p++)
+//     if(couleurChoisie[p].checked == false){
+//         return couleurChoisie[p].id
+//     }
+// }
 
-    let nouvelArticle = new ArticlePAnier(2, "beige", 2)
+//     let nouvelArticle = new ArticlePAnier(2, "beige", 2)
 
-}
+// }
 
